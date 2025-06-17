979484a2b50f28e9e168d12824fececda0114f54
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2025-06-17 12:58:42 +0300

    add ability to size datagrid columns

--- src/resources/views/crud/components/datagrid.blade.php
+++ src/resources/views/crud/components/datagrid.blade.php
@@ -1,17 +1,17 @@
-<div class="datagrid p-3">
+<div class="bp-datagrid p-3">
     @foreach($columns as $column)
-        <div class="datagrid-item">
-            <div class="datagrid-title">{!! $column['label'] !!}</div>
-            <div class="datagrid-content">
+        <div class="bp-datagrid-item size-{{ $column['size'] ?? '3' }}">
+            <div class="bp-datagrid-title">{!! $column['label'] !!}</div>
+            <div class="bp-datagrid-content">
                 @includeFirst(\Backpack\CRUD\ViewNamespaces::getViewPathsWithFallbackFor('columns', $column['type'], 'crud::columns.text'))
             </div>
         </div>
     @endforeach
 
     @if($crud && $crud->buttons()->where('stack', 'line')->count() && $displayActionsColumn)
-        <div class="datagrid-item">
-            <div class="datagrid-title">{{ trans('backpack::crud.actions') }}</div>
-            <div class="datagrid-content">
+        <div class="bp-datagrid-item size-12">
+            <div class="bp-datagrid-title">{{ trans('backpack::crud.actions') }}</div>
+            <div class="bp-datagrid-content">
                 @include('crud::inc.button_stack', ['stack' => 'line'])
             </div>
         </div>
