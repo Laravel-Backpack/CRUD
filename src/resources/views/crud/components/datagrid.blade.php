64b8fae9bda5f3c470608f617b252e59d3ff3f49
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2025-06-17 12:01:08 +0300

    remove view processing from datalist and datacomponent blade files

--- src/resources/views/crud/components/datagrid.blade.php
+++ src/resources/views/crud/components/datagrid.blade.php
@@ -3,20 +3,7 @@
         <div class="datagrid-item">
             <div class="datagrid-title">{!! $column['label'] !!}</div>
             <div class="datagrid-content">
-                @php
-                // create a list of paths to column blade views
-                // including the configured view_namespaces
-                $columnPaths = array_map(function($item) use ($column) {
-                    return $item.'.'.$column['type'];
-                }, \Backpack\CRUD\ViewNamespaces::getFor('columns'));
-
-                // but always fall back to the stock 'text' column
-                // if a view doesn't exist
-                if (!in_array('crud::columns.text', $columnPaths)) {
-                    $columnPaths[] = 'crud::columns.text';
-                }
-                @endphp
-                @includeFirst($columnPaths)
+                @includeFirst(\Backpack\CRUD\ViewNamespaces::getViewPathsWithFallbackFor('columns', $column['type'], 'crud::columns.text'))
             </div>
         </div>
     @endforeach
--- src/resources/views/crud/components/datalist.blade.php
+++ src/resources/views/crud/components/datalist.blade.php
@@ -6,20 +6,7 @@
                 <strong>{!! $column['label'] !!}@if(!empty($column['label'])):@endif</strong>
             </td>
             <td @if($loop->index === 0) class="border-top-0" @endif>
-                @php
-                // create a list of paths to column blade views
-                // including the configured view_namespaces
-                $columnPaths = array_map(function($item) use ($column) {
-                    return $item.'.'.$column['type'];
-                }, \Backpack\CRUD\ViewNamespaces::getFor('columns'));
-
-                // but always fall back to the stock 'text' column
-                // if a view doesn't exist
-                if (!in_array('crud::columns.text', $columnPaths)) {
-                    $columnPaths[] = 'crud::columns.text';
-                }
-                @endphp
-                @includeFirst($columnPaths)
+                @includeFirst(\Backpack\CRUD\ViewNamespaces::getViewPathsWithFallbackFor('columns', $column['type'], 'crud::columns.text'))
             </td>
         </tr>
         @endforeach
