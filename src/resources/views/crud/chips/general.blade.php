fc0ec7aa43b61cbd6f4180e2f60d7bec583c820c
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2025-06-21 11:18:30 +0300

    added general chip

---
+++ src/resources/views/crud/chips/general.blade.php
@@ -0,0 +1,48 @@
+@php
+    $text = $text ?? null;
+    $title = $title ?? null;
+    $target = $target ?? "_self";
+    $url = $url ?? null;
+    $details = $details ?? [];
+    $image = $image ?? null;
+    $showImage = isset($showImage) ? $showImage : !empty($image);
+@endphp
+
+<div class="row align-items-center">
+    @if ($showImage)
+        <div class="col-auto">
+            <div class="d-block">
+                @if ($url)
+                    <a href="{{ $url }}" title="{{ $title }}" target="{{ $target }}" class="d-inline-block">
+                @endif
+                @if ($image)
+                <span class="avatar avatar-2 rounded" style="background-image: url({{ $image }})"> </span>
+                @else
+                <span class="avatar avatar-2 rounded bg-secondary text-white">
+                    {{ $title ? mb_substr($title, 0, 1, 'UTF-8') : 'A' }}
+                </span>
+                @endif
+                @if ($url)
+                    </a>
+                @endif
+            </div>
+        </div>
+    @endif
+    <div class="col text-truncate">
+        <div class="d-block">
+            <a @if ($url) href="{{ $url }}" @endif class="mb-1 d-inline-block @if (!$url) text-dark @endif" title="{{ $title }}" target="{{ $target }}">
+                {{ $text }}
+            </a>
+        </div>
+        <div class="d-block text-secondary text-truncate mt-n1">
+            @foreach ($details as $key => $detail)
+                <small class="d-inline-block me-1">
+                    <i class="{{ $detail['icon'] }}" title="{{ $detail['title'] ?? '' }}"></i>
+                    <a @if (isset($detail['url']) && $detail['url'] != null) href="{{ $detail['url'] }}" @endif
+                        class="text-reset"
+                        title="{{ $detail['title'] ?? '' }}">{{ $detail['text'] }}</a>
+                </small>
+            @endforeach
+        </div>
+    </div>
+</div>
