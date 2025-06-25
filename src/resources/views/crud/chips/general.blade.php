a54221de338bca9cf3fcb6f0b19761081c38fcd0
Cristian Tabacitu <cristian.tabacitu@digitallyhappy.com>
2025-06-25 12:54:27 +0300

    change general chip syntax

--- src/resources/views/crud/chips/general.blade.php
+++ src/resources/views/crud/chips/general.blade.php
@@ -1,53 +1,83 @@
 @php
-    // the main text showing in the chip
-    $text = $text ?? null;
-    $title = $title ?? null;
+    $defaultHeading = [
+        'content' => null, // text to show in the heading
+        'element' => 'a',
+        'class' => 'mb-1 d-inline-block',
+    ];
 
-    // the URL for the main text and image (if any)
-    $url = $url ?? null;
-    $target = $target ?? "_self";
+    $defaultImage = [
+        'content' => null, // image url
+        'element' => 'a',
+        'class' => 'avatar avatar-2 rounded',
+    ];
 
-    // the image (if any)
-    $image = $image ?? null;
-    $showImage = isset($showImage) ? $showImage : !empty($image);
+    // merge any passed parameters with the defaults
+    $heading = array_merge($defaultHeading, $heading ?? []); // the main heading showing in the chip
+    $image = array_merge($defaultImage, $image ?? []); // the image that shows up in the chip (if any)
+    $details = $details ?? []; // the details that show up on the second row (if any)
 
-    // the details that show up on the second row (if any)
-    $details = $details ?? [];
+    // ensure the details have the minimum info
+    foreach ($details as $key => $detail) {
+        $details[$key]['element'] = $detail['element'] ?? 'span';
+        $details[$key]['class'] = $detail['class'] ?? 'text-reset';
+    }
+
+    // if the heading has a href, target and tile, and the image does not
+    // then use those for the image as well
+    if ($image['content'] !== null && $heading['content'] !== null) {
+        $image['href'] = $image['href'] ?? $heading['href'] ?? null;
+        $image['title'] = $image['title'] ?? $heading['title'] ?? null;
+        $image['target'] = $image['target'] ?? $heading['target'] ?? null;
+    }
 @endphp
 
 <div class="row align-items-center bp-chip">
-    @if ($showImage)
+    @if ($image['content'])
         <div class="col-auto">
             <div class="d-block">
-                @if ($url)
-                    <a href="{{ $url }}" title="{{ $title }}" target="{{ $target }}" class="d-inline-block">
-                @endif
-                @if ($image)
-                <span class="avatar avatar-2 rounded" style="background-image: url({{ $image }})"> </span>
-                @else
-                <span class="avatar avatar-2 rounded bg-secondary text-white">
-                    {{ $title ? mb_substr($title, 0, 1, 'UTF-8') : 'A' }}
-                </span>
-                @endif
-                @if ($url)
-                    </a>
+                @if ($image['content'])
+                    <{{ $image['element'] }}
+                        @foreach ($image as $attribute => $value)
+                            @if ($attribute !== 'element' && $attribute !== 'content')
+                                {{ $attribute }}="{{ $value }}"
+                            @endif
+                        @endforeach
+                    >
+                        <span class="avatar avatar-2 rounded" style="background-image: url({{ $image['content'] }})"> </span>
+                    </{{ $image['element'] }}>
                 @endif
             </div>
         </div>
     @endif
     <div class="col text-truncate">
         <div class="d-block">
-            <a @if ($url) href="{{ $url }}" @endif class="mb-1 d-inline-block @if (!$url) text-dark @endif" title="{{ $title }}" target="{{ $target }}">
-                {{ $text }}
-            </a>
+            @if ($heading['content'])
+                <{{ $heading['element'] }}
+                    @foreach ($heading as $attribute => $value)
+                        @if ($attribute !== 'element' && $attribute !== 'content')
+                            {{ $attribute }}="{{ $value }}"
+                        @endif
+                    @endforeach
+                >
+                    {{ $heading['content'] }}
+                </{{ $heading['element'] }}>
+            @endif
         </div>
         <div class="d-block text-secondary text-truncate mt-n1">
             @foreach ($details as $key => $detail)
                 <small class="d-inline-block me-1">
+                    @if (isset($detail['icon']))
                     <i class="{{ $detail['icon'] }}" title="{{ $detail['title'] ?? '' }}"></i>
-                    <a @if (isset($detail['url']) && $detail['url'] != null) href="{{ $detail['url'] }}" @endif
-                        class="text-reset"
-                        title="{{ $detail['title'] ?? '' }}">{{ $detail['text'] }}</a>
+                    @endif
+                    <{{ $detail['element'] }}
+                        @foreach ($detail as $attribute => $value)
+                            @if ($attribute !== 'element' && $attribute !== 'icon' && $attribute !== 'content')
+                                {{ $attribute }}="{{ $value }}"
+                            @endif
+                        @endforeach
+                        >
+                        {{ $detail['content'] }}
+                    </{{ $detail['element'] }}>
                 </small>
             @endforeach
         </div>
