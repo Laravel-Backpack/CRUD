28bddff129fd335c16c4da90618fc684904e5637
James Fenwick <james.fenwick@iamproperty.com>
2025-07-29 14:48:49 +0100

    Escape heading entity names in datatable component

--- src/resources/views/crud/components/datatable/datatable.blade.php
+++ src/resources/views/crud/components/datatable/datatable.blade.php
@@ -3,9 +3,10 @@
     $tableId = $tableId ?? 'crudTable';
 @endphp
 <section class="header-operation datatable-header animated fadeIn d-flex mb-2 align-items-baseline d-print-none" bp-section="page-header">
-          <h1 class="text-capitalize mb-0" bp-section="page-heading">{!! $crud->getHeading() ?? $crud->entity_name_plural !!}</h1>
-          <p class="ms-2 ml-2 mb-0" id="datatable_info_stack_{{$tableId}}" bp-section="page-subheading">{!! $crud->getSubheading() ?? '' !!}</p>
-        </section>
+  <h1 class="text-capitalize mb-0" bp-section="page-heading">{{ $crud->getHeading() ?? $crud->entity_name_plural }}</h1>
+  <p class="ms-2 ml-2 mb-0" id="datatable_info_stack_{{$tableId}}" bp-section="page-subheading">{{ $crud->getSubheading() ?? '' }}</p>
+</section>
+
 <div class="row mb-2 align-items-center">
   <div class="col-sm-9">
     @if ( $crud->buttons()->where('stack', 'top')->count() ||  $crud->exportButtons())
