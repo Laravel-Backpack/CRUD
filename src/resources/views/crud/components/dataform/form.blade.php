dc68fa4b8ac4714a9bde7eb107f21328adc8126b
pxpm <pxpm88@gmail.com>
2025-10-24 16:20:07 +0100

    wip

--- src/resources/views/crud/components/dataform/form.blade.php
+++ src/resources/views/crud/components/dataform/form.blade.php
@@ -1,5 +1,5 @@
 <div class="backpack-form">
-    @include('crud::inc.grouped_errors', ['id' => $formId])
+    @include('crud::inc.grouped_errors', ['formId' => $formId])
 
     <form method="post"
         action="{{ $formAction }}"
@@ -11,7 +11,7 @@
             @method($formMethod)
         @endif
         {{-- Include the form fields --}}
-        @include('crud::form_content', ['fields' => $crud->fields(), 'action' => $formOperation, 'id' => $id])
+        @include('crud::form_content', ['fields' => $crud->fields(), 'action' => $formOperation, 'formId' => $formId])
 
         {{-- This makes sure that all field assets are loaded. --}}
         <div class="d-none" id="parentLoadedAssets">{{ json_encode(Basset::loaded()) }}</div>
