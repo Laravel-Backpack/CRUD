aba39014a81c491c58450177e80638df187aab70
pxpm <pxpm88@gmail.com>
2026-04-09 10:55:38 +0100

    delete report button

--- src/resources/views/crud/buttons/report.blade.php
+++
@@ -1,5 +0,0 @@
-@if ($crud->hasAccess('report'))
-    <a href="{{ url($crud->route.'/report') }}" class="btn btn-outline-info" bp-button="report">
-        <i class="la la-chart-bar"></i> <span>Report</span>
-    </a>
-@endif
