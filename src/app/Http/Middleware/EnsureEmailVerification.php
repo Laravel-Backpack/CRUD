056fcd831eb4a6741764d6160551fe796e90a990
pxpm <pxpm88@gmail.com>
2026-08-10 14:20:17 +0100

    Add email_verification_route_names config and read it in EnsureEmailVerification middleware

--- src/app/Http/Middleware/EnsureEmailVerification.php
+++ src/app/Http/Middleware/EnsureEmailVerification.php
@@ -17,8 +17,14 @@ class EnsureEmailVerification
      */
     public function handle($request, Closure $next)
     {
+        $names = config('backpack.base.email_verification_route_names', [
+            'notice' => 'verification.notice',
+            'verify' => 'verification.verify',
+            'send' => 'verification.send',
+        ]);
+
         // if the route is one in the verification process, do nothing
-        if (in_array($request->route()->getName(), ['verification.notice', 'verification.verify', 'verification.send'])) {
+        if (in_array($request->route()->getName(), $names)) {
             return $next($request);
         }
 
@@ -34,6 +40,6 @@ class EnsureEmailVerification
             throw new Exception('Missing "verified" alias middleware in App/Http/Kernel.php. More info: https://backpackforlaravel.com/docs/6.x/base-how-to#enable-email-verification-in-backpack-routes');
         }
 
-        return $verifiedMiddleware->handle($request, $next);
+        return $verifiedMiddleware->handle($request, $next, $names['notice']);
     }
 }
